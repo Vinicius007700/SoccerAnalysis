839d4e08fb883cb4b3f7d140e466a61b710856b6
Vinícius Maciel <v281391@dac.unicamp.br>
2026-01-26 15:48:47 -0300

    Colocadas classes para a limpeza do código

--- assets/draw_game.py
+++ assets/draw_game.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 import pandas as pd
 
@@ -6,146 +7,208 @@ field_l = 106.0
 field_w = 68.0
 FPS = 25
 
-def plot_pitch( field_dimen = (106.0,68.0), field_color ='green', linewidth=2, markersize=20):
-    """ plot_pitch
-    
-    Plots a soccer pitch. All distance units converted to meters.
-    
-    Parameters
-    -----------
-        field_dimen: (length, width) of field in meters. Default is (106,68)
-        field_color: color of field. options are {'green','white'}
-        linewidth  : width of lines. default = 2
-        markersize : size of markers (e.g. penalty spot, centre spot, posts). default = 20
+class MatchAnimator:
+    def __init__(self, dataset, events, home_team, away_team):
+        self.df = dataset.to_df(engine="pandas")
+        self.events_df = events.to_df(engine="pandas")
         
-    Returrns
-    -----------
-       fig,ax : figure and aixs objects (so that other data can be plotted onto the pitch)
-
-    """
-    fig,ax = plt.subplots(figsize=(12,8)) # create a figure 
-    # decide what color we want the field to be. Default is green, but can also choose white
-    if field_color=='green':
-        ax.set_facecolor('mediumseagreen')
-        lc = 'whitesmoke' # line color
-        pc = 'w' # 'spot' colors
-    elif field_color=='white':
-        lc = 'k'
-        pc = 'k'
-    # ALL DIMENSIONS IN m
-    border_dimen = (3,3) # include a border arround of the field of width 3m
-    meters_per_yard = 0.9144 # unit conversion from yards to meters
-    half_pitch_length = field_dimen[0]/2. # length of half pitch
-    half_pitch_width = field_dimen[1]/2. # width of half pitch
-    signs = [-1,1] 
-    # Soccer field dimensions typically defined in yards, so we need to convert to meters
-    goal_line_width = 8*meters_per_yard
-    box_width = 20*meters_per_yard
-    box_length = 6*meters_per_yard
-    area_width = 44*meters_per_yard
-    area_length = 18*meters_per_yard
-    penalty_spot = 12*meters_per_yard
-    corner_radius = 1*meters_per_yard
-    D_length = 8*meters_per_yard
-    D_radius = 10*meters_per_yard
-    D_pos = 12*meters_per_yard
-    centre_circle_radius = 10*meters_per_yard
-    # plot half way line # center circle
-    ax.plot([0,0],[-half_pitch_width,half_pitch_width],lc,linewidth=linewidth)
-    ax.scatter(0.0,0.0,marker='o',facecolor=lc,linewidth=0,s=markersize)
-    y = np.linspace(-1,1,50)*centre_circle_radius
-    x = np.sqrt(centre_circle_radius**2-y**2)
-    ax.plot(x,y,lc,linewidth=linewidth)
-    ax.plot(-x,y,lc,linewidth=linewidth)
-    for s in signs: # plots each line seperately
-        # plot pitch boundary
-        ax.plot([-half_pitch_length,half_pitch_length],[s*half_pitch_width,s*half_pitch_width],lc,linewidth=linewidth)
-        ax.plot([s*half_pitch_length,s*half_pitch_length],[-half_pitch_width,half_pitch_width],lc,linewidth=linewidth)
-        # goal posts & line
-        ax.plot( [s*half_pitch_length,s*half_pitch_length],[-goal_line_width/2.,goal_line_width/2.],pc+'s',markersize=6*markersize/20.,linewidth=linewidth)
-        # 6 yard box
-        ax.plot([s*half_pitch_length,s*half_pitch_length-s*box_length],[box_width/2.,box_width/2.],lc,linewidth=linewidth)
-        ax.plot([s*half_pitch_length,s*half_pitch_length-s*box_length],[-box_width/2.,-box_width/2.],lc,linewidth=linewidth)
-        ax.plot([s*half_pitch_length-s*box_length,s*half_pitch_length-s*box_length],[-box_width/2.,box_width/2.],lc,linewidth=linewidth)
-        # penalty area
-        ax.plot([s*half_pitch_length,s*half_pitch_length-s*area_length],[area_width/2.,area_width/2.],lc,linewidth=linewidth)
-        ax.plot([s*half_pitch_length,s*half_pitch_length-s*area_length],[-area_width/2.,-area_width/2.],lc,linewidth=linewidth)
-        ax.plot([s*half_pitch_length-s*area_length,s*half_pitch_length-s*area_length],[-area_width/2.,area_width/2.],lc,linewidth=linewidth)
-        # penalty spot
-        ax.scatter(s*half_pitch_length-s*penalty_spot,0.0,marker='o',facecolor=lc,linewidth=0,s=markersize)
-        # corner flags
-        y = np.linspace(0,1,50)*corner_radius
-        x = np.sqrt(corner_radius**2-y**2)
-        ax.plot(s*half_pitch_length-s*x,-half_pitch_width+y,lc,linewidth=linewidth)
-        ax.plot(s*half_pitch_length-s*x,half_pitch_width-y,lc,linewidth=linewidth)
-        # draw the D
-        y = np.linspace(-1,1,50)*D_length # D_length is the chord of the circle that defines the D
-        x = np.sqrt(D_radius**2-y**2)+D_pos
-        ax.plot(s*half_pitch_length-s*x,y,lc,linewidth=linewidth)
+        self.home_cols_x = []
+        self.home_cols_y = []
+        self.away_cols_x = []
+        self.away_cols_y = []
         
-    # remove axis labels and ticks
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    ax.set_xticks([])
-    ax.set_yticks([])
-    # set axis limits
-    xmax = field_dimen[0]/2. + border_dimen[0]
-    ymax = field_dimen[1]/2. + border_dimen[1]
-    ax.set_xlim([-xmax,xmax])
-    ax.set_ylim([-ymax,ymax])
-    ax.set_axisbelow(True)
-    return fig,ax
+        self.player_jersey_map = {}
+        self.player_color_map = {}
+        
+        for player in home_team.players: 
+            self.player_color_map[player.player_id] = 'cyan'
+            self.player_jersey_map[player.player_id] = player.jersey_no
 
+        for player in away_team.players: 
+            self.player_color_map[player.player_id] = 'red'
+            self.player_jersey_map[player.player_id] = player.jersey_no
+        
+        for col in self.df.columns:
+            if '_x' in col and 'ball' not in col:
+                player_id = col.replace('_x', '')
+                y_col = col.replace('_x', '_y')
+                
+                color = self.player_color_map.get(player_id)
+                if color == 'cyan':
+                    self.home_cols_x.append(col)
+                    self.home_cols_y.append(y_col)
+                elif color == 'red':
+                    self.away_cols_x.append(col)
+                    self.away_cols_y.append(y_col)
+                    
+                    
+        self.fig, self.ax = self._plot_pitch()
+        
+        self.scat_home = self.ax.scatter([],[], c='red', s=120, edgecolors='white', zorder=5, label='Casa')
+        self.scat_away = self.ax.scatter([],[], c='cyan', s=120, edgecolors='black', zorder=5, label='Fora')
+        
+        self.scat_ball = self.ax.scatter([],[], c='white', s=100, edgecolors='black', linewidth=1.5, zorder=10)
+        self.home_texts = [self.ax.text(0, 0, '', color='white', fontweight='bold', ha='center', va='center', fontsize=8, zorder=6) for _ in range(len(self.home_cols_x))]
+        self.away_texts = [self.ax.text(0, 0, '', color='black', fontweight='bold', ha='center', va='center', fontsize=8, zorder=6) for _ in range(len(self.away_cols_x))]
 
-def set_teams_on_board(row, team_cols_x, team_cols_y, scat_team, team_texts, player_jersey_map):
-    t_x_raw = row[team_cols_x].values.astype(float)
-    t_y_raw = row[team_cols_y].values.astype(float)
+        self.title_text = self.ax.text(0.5, 1.02, "", transform=self.ax.transAxes, color='white', ha='center', fontsize=12)
+        
+    def update(self, frame_idx):
+        row = self.df.iloc[frame_idx]
+        self._set_teams_on_board(row, self.home_cols_x, self.home_cols_y, self.scat_home, 
+                           self.home_texts)
     
-    t_x = (t_x_raw - 0.5) * field_l
-    t_y = (t_y_raw - 0.5) * field_w
-    
-    scat_team.set_offsets(np.column_stack((t_x, t_y)))
-    for text, x, y, col_name in zip(team_texts, t_x, t_y, team_cols_x):
-        if np.isnan(x) or np.isnan(y):
-            text.set_visible(False)
-        else:
-            text.set_visible(True)
-            text.set_position((x, y))
-            p_id = col_name.replace('_x', '')
-            text.set_text(str(player_jersey_map.get(p_id, '')))
+        # Time Visitante
+        self._set_teams_on_board(row, self.away_cols_x, self.away_cols_y, self.scat_away, 
+                           self.away_texts)
             
-          
-def set_ball_on_board(row, scat_ball, df):
-    if 'ball_x' in df.columns:
-        # Pega valor escalar, converte se for None
-        bx_val = row['ball_x']
-        by_val = row['ball_y']
+        # --- Bola ---
+        self._set_ball_on_board(row)
         
-        # Se for NaN (bola fora de jogo/tracking perdido), joga pra longe
-        if pd.isna(bx_val) or pd.isna(by_val):
-             scat_ball.set_offsets(np.array([[-100, -100]]))
-        else:
-             b_x = (float(bx_val) - 0.5) * field_l
-             b_y = (float(by_val) - 0.5) * field_w
-             scat_ball.set_offsets(np.array([[b_x, b_y]]))
-
-
-
-def update(frame_idx, df, home_cols_x, home_cols_y, away_cols_x, away_cols_y, scat_home, scat_away, scat_ball, home_texts, away_texts,
-           player_jersey_map, title_text ):
-    
-    row = df.iloc[frame_idx]
-    
-
-    #Time da Casa
-    set_teams_on_board(row, home_cols_x, home_cols_y, scat_home, home_texts, player_jersey_map)
-    
-    # Time Visitante
-    set_teams_on_board(row, away_cols_x, away_cols_y, scat_away, away_texts, player_jersey_map)
+        self.title_text.set_text(f"Frame {frame_idx}")
         
-    # --- Bola ---
-    set_ball_on_board(row, scat_ball, df)
+        return self.scat_home, self.scat_away, self.scat_ball, self.title_text, *self.home_texts, *self.away_texts
     
-    title_text.set_text(f"Frame {frame_idx}")
     
-    return scat_home, scat_away, scat_ball, title_text, *home_texts, *away_texts
+    
+    def _set_teams_on_board(self, row, team_cols_x, team_cols_y, scat_team, team_texts):
+        t_x_raw = row[team_cols_x].values.astype(float)
+        t_y_raw = row[team_cols_y].values.astype(float)
+        
+        t_x = (t_x_raw - 0.5) * field_l
+        t_y = (t_y_raw - 0.5) * field_w
+        
+        scat_team.set_offsets(np.column_stack((t_x, t_y)))
+        for text, x, y, col_name in zip(team_texts, t_x, t_y, team_cols_x):
+            if np.isnan(x) or np.isnan(y):
+                text.set_visible(False)
+            else:
+                text.set_visible(True)
+                text.set_position((x, y))
+                p_id = col_name.replace('_x', '')
+                text.set_text(str(self.player_jersey_map.get(p_id, '')))
+                
+            
+    def _set_ball_on_board(self, row):
+        if 'ball_x' in self.df.columns:
+            # Pega valor escalar, converte se for None
+            bx_val = row['ball_x']
+            by_val = row['ball_y']
+            
+            # Se for NaN (bola fora de jogo/tracking perdido), joga pra longe
+            if pd.isna(bx_val) or pd.isna(by_val):
+                self.scat_ball.set_offsets(np.array([[-100, -100]]))
+            else:
+                b_x = (float(bx_val) - 0.5) * field_l
+                b_y = (float(by_val) - 0.5) * field_w
+                self.scat_ball.set_offsets(np.array([[b_x, b_y]]))
+        
+
+    def _plot_pitch(self, field_dimen = (106.0,68.0), field_color ='green', linewidth=2, markersize=20):
+        """ plot_pitch
+        
+        Plots a soccer pitch. All distance units converted to meters.
+        
+        Parameters
+        -----------
+            field_dimen: (length, width) of field in meters. Default is (106,68)
+            field_color: color of field. options are {'green','white'}
+            linewidth  : width of lines. default = 2
+            markersize : size of markers (e.g. penalty spot, centre spot, posts). default = 20
+            
+        Returrns
+        -----------
+        fig,ax : figure and aixs objects (so that other data can be plotted onto the pitch)
+
+        """
+        fig,ax = plt.subplots(figsize=(12,8)) # create a figure 
+        # decide what color we want the field to be. Default is green, but can also choose white
+        if field_color=='green':
+            ax.set_facecolor('mediumseagreen')
+            lc = 'whitesmoke' # line color
+            pc = 'w' # 'spot' colors
+        elif field_color=='white':
+            lc = 'k'
+            pc = 'k'
+        # ALL DIMENSIONS IN m
+        border_dimen = (3,3) # include a border arround of the field of width 3m
+        meters_per_yard = 0.9144 # unit conversion from yards to meters
+        half_pitch_length = field_dimen[0]/2. # length of half pitch
+        half_pitch_width = field_dimen[1]/2. # width of half pitch
+        signs = [-1,1] 
+        # Soccer field dimensions typically defined in yards, so we need to convert to meters
+        goal_line_width = 8*meters_per_yard
+        box_width = 20*meters_per_yard
+        box_length = 6*meters_per_yard
+        area_width = 44*meters_per_yard
+        area_length = 18*meters_per_yard
+        penalty_spot = 12*meters_per_yard
+        corner_radius = 1*meters_per_yard
+        D_length = 8*meters_per_yard
+        D_radius = 10*meters_per_yard
+        D_pos = 12*meters_per_yard
+        centre_circle_radius = 10*meters_per_yard
+        # plot half way line # center circle
+        ax.plot([0,0],[-half_pitch_width,half_pitch_width],lc,linewidth=linewidth)
+        ax.scatter(0.0,0.0,marker='o',facecolor=lc,linewidth=0,s=markersize)
+        y = np.linspace(-1,1,50)*centre_circle_radius
+        x = np.sqrt(centre_circle_radius**2-y**2)
+        ax.plot(x,y,lc,linewidth=linewidth)
+        ax.plot(-x,y,lc,linewidth=linewidth)
+        for s in signs: # plots each line seperately
+            # plot pitch boundary
+            ax.plot([-half_pitch_length,half_pitch_length],[s*half_pitch_width,s*half_pitch_width],lc,linewidth=linewidth)
+            ax.plot([s*half_pitch_length,s*half_pitch_length],[-half_pitch_width,half_pitch_width],lc,linewidth=linewidth)
+            # goal posts & line
+            ax.plot( [s*half_pitch_length,s*half_pitch_length],[-goal_line_width/2.,goal_line_width/2.],pc+'s',markersize=6*markersize/20.,linewidth=linewidth)
+            # 6 yard box
+            ax.plot([s*half_pitch_length,s*half_pitch_length-s*box_length],[box_width/2.,box_width/2.],lc,linewidth=linewidth)
+            ax.plot([s*half_pitch_length,s*half_pitch_length-s*box_length],[-box_width/2.,-box_width/2.],lc,linewidth=linewidth)
+            ax.plot([s*half_pitch_length-s*box_length,s*half_pitch_length-s*box_length],[-box_width/2.,box_width/2.],lc,linewidth=linewidth)
+            # penalty area
+            ax.plot([s*half_pitch_length,s*half_pitch_length-s*area_length],[area_width/2.,area_width/2.],lc,linewidth=linewidth)
+            ax.plot([s*half_pitch_length,s*half_pitch_length-s*area_length],[-area_width/2.,-area_width/2.],lc,linewidth=linewidth)
+            ax.plot([s*half_pitch_length-s*area_length,s*half_pitch_length-s*area_length],[-area_width/2.,area_width/2.],lc,linewidth=linewidth)
+            # penalty spot
+            ax.scatter(s*half_pitch_length-s*penalty_spot,0.0,marker='o',facecolor=lc,linewidth=0,s=markersize)
+            # corner flags
+            y = np.linspace(0,1,50)*corner_radius
+            x = np.sqrt(corner_radius**2-y**2)
+            ax.plot(s*half_pitch_length-s*x,-half_pitch_width+y,lc,linewidth=linewidth)
+            ax.plot(s*half_pitch_length-s*x,half_pitch_width-y,lc,linewidth=linewidth)
+            # draw the D
+            y = np.linspace(-1,1,50)*D_length # D_length is the chord of the circle that defines the D
+            x = np.sqrt(D_radius**2-y**2)+D_pos
+            ax.plot(s*half_pitch_length-s*x,y,lc,linewidth=linewidth)
+            
+        # remove axis labels and ticks
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_xticks([])
+        ax.set_yticks([])
+        # set axis limits
+        xmax = field_dimen[0]/2. + border_dimen[0]
+        ymax = field_dimen[1]/2. + border_dimen[1]
+        ax.set_xlim([-xmax,xmax])
+        ax.set_ylim([-ymax,ymax])
+        ax.set_axisbelow(True)
+        return fig,ax
+    
+    def runMatch(self, start_frame, end_frame):
+        return animation.FuncAnimation(
+            self.fig, 
+            self.update, 
+            frames=range(start_frame, end_frame), 
+            interval=40, 
+            blit=True
+        )
+
+class Team():
+    def __init__(self):
+        pass
+        
+        
+    
+
+
+
--- assets/main.py
+++ assets/main.py
@@ -32,7 +32,8 @@ event_path = os.path.join(root_folder, 'data', 'Sample_Game_3_events.json')
 dataset = metrica.load_tracking_epts(
     meta_data=metadata_path,
     raw_data=tracking_path,
-    coordinates="metrica"
+    coordinates="metrica",
+    limit= 1000
 )
 event_dataset = metrica.load_event(
     event_data = event_path,
@@ -53,6 +54,8 @@ if len(gols) > 0:
 else:
     time_gol = 500
     print('Nenhum gol encontrado')
+    
+time_gol = 500
 
 
 
@@ -60,77 +63,32 @@ else:
 home_team = dataset.metadata.teams[0]
 away_team = dataset.metadata.teams[1]
 
-player_jersey_map = {}
-player_color_map = {}
 
 
-for player in home_team.players: 
-    player_color_map[player.player_id] = 'cyan'
-    player_jersey_map[player.player_id] = player.jersey_no
-
-for player in away_team.players: 
-    player_color_map[player.player_id] = 'red'
-    player_jersey_map[player.player_id] = player.jersey_no
 
 
 df = dataset.to_df(engine="pandas")
 print(df.head())
 
 # Criar listas com os nomes das colunas de cada time para acesso rápido
-home_cols_x = []
-home_cols_y = []
-away_cols_x = []
-away_cols_y = []
 
-for col in df.columns:
-    if '_x' in col and 'ball' not in col:
-        player_id = col.replace('_x', '')
-        y_col = col.replace('_x', '_y')
-        
-        color = player_color_map.get(player_id)
-        if color == 'cyan':
-            home_cols_x.append(col)
-            home_cols_y.append(y_col)
-        elif color == 'red':
-            away_cols_x.append(col)
-            away_cols_y.append(y_col)
+
+
 
 #desenhar o campo
-fig, ax = dg.plot_pitch()
+
 
 # Criar os objetos "Atores" vazios (que vão se mover)
 # scatter retorna um objeto que podemos atualizar depois
-scat_home = ax.scatter([],[], c='red', s=120, edgecolors='white', zorder=5, label='Casa')
-scat_away = ax.scatter([],[], c='cyan', s=120, edgecolors='black', zorder=5, label='Fora')
-scat_ball = ax.scatter([],[], c='white', s=100, edgecolors='black', linewidth=1.5, zorder=10)
 
-home_texts = [ax.text(0, 0, '', color='white', fontweight='bold', ha='center', va='center', fontsize=8, zorder=6) for _ in range(len(home_cols_x))]
-away_texts = [ax.text(0, 0, '', color='black', fontweight='bold', ha='center', va='center', fontsize=8, zorder=6) for _ in range(len(away_cols_x))]
 
-title_text = ax.text(0.5, 1.02, "", transform=ax.transAxes, color='white', ha='center', fontsize=12)
+ani = dg.MatchAnimator(dataset, event_dataset, home_team, away_team)
+my_ani = ani.runMatch(time_gol - 100, time_gol + 100)
 
 
 
 
 
-
-update_arguments = (
-    df, 
-    home_cols_x, home_cols_y, 
-    away_cols_x, away_cols_y, 
-    scat_home, scat_away, scat_ball, 
-    home_texts, away_texts, 
-    player_jersey_map, 
-    title_text
-)
-ani = animation.FuncAnimation(
-    fig, 
-    dg.update, 
-    frames=range(time_gol - 100, time_gol + 100), 
-    fargs=update_arguments, 
-    interval=40, 
-    blit=True
-)
 
 # # Salvar como MP4
 # output_file = os.path.join(current_folder, 'match_animation.mp4')
